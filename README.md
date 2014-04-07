205106602fdba338989816fca14e25c03e623345
Carsten Witt <carsten.witt@gmail.com>
2014-04-07 11:04:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Cookies
+
+Small PHP library for dealing with cookies.
+
+##Testing
+There is a PHPUnit test suite boilerplate in the `tests` directory, neither used nor implemented. Yet.
+
+
+
+
+
+
---
+++ src/Cookie.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of tomkyle/cookies.
+ *
+ * Copyright (c) 2014 Carsten Witt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace tomkyle\Cookies;
+
+
+/**
+ * Cookie
+ */
+class Cookie extends CookieAbstract implements CookieInterface
+{
+
+    /**
+     * @param string $name
+     * @param mixed  $value Default: `null`
+     * @param DateTimeInterface $valid_until Instance of `DateTimeInterface`, default: `null`
+     *
+     * @uses setName()
+     * @uses setValue()
+     * @uses setValidUntil()
+     */
+    public function __construct($name, $value = null, \DateTimeInterface $valid_until = null)
+    {
+        $this->setName(  $name );
+        $this->setValue( $value );
+        $this->setValidUntil( $valid_until );
+    }
+
+
+    public function __toString()
+    {
+        return $this->getValue() ?: '';
+    }
+
+
+
+
+}
